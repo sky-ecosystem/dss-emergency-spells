82ce782d0f6ee3457f9ef046fde02bae2964297f
Riccardo Persiani <r.persiani92@gmail.com>
2026-01-23 12:08:08 +0100

    wip(scripts/usds): first draft

---
+++ script/StUsdsDissRateSetterBudDeploy.s.sol
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {ScriptTools} from "dss-test/ScriptTools.sol";
+import {StUsdsDissRateSetterBudFactory} from "src/stusds/StUsdsDissRateSetterBudSpell.sol";
+
+
+contract SingleOsmStopDeployScript is Script {
+    using stdJson for string;
+    using ScriptTools for string;
+
+    string constant NAME = "stusds-diss-rate-bud-setter-deploy";
+    string config;
+
+    StUsdsDissRateSetterBudFactory fab;
+    string[] budStrs;
+
+    function run() external {
+        config = ScriptTools.loadConfig();
+
+        fab = StUsdsDissRateSetterBudFactory(config.readAddress(".factory", "FOUNDRY_FACTORY"));
+        budStrs = config.readStringArray(".buds", "FOUNDRY_BUDS");
+
+        vm.startBroadcast();
+
+        for (uint256 i = 0; i < budStrs.length; i++) {
+            address bud = vm.parseAddress(budStrs[i]);
+            address spell = fab.deploy(bud);
+            ScriptTools.exportContract(NAME, budStrs[i], spell);
+        }
+        vm.stopBroadcast();
+    }
+}
---
+++ script/StUsdsSingleLineOrCapWipeDeploy.s.sol
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {ScriptTools} from "dss-test/ScriptTools.sol";
+import {StUsdsSingleLineOrCapWipeFactory, Flow} from "src/stusds/StUsdsSingleLineOrCapWipeSpell.sol";
+
+interface LitePsmLike {
+    function ilk() external view returns (bytes32);
+}
+
+contract StUsdsSingleLineOrCapWipeDeployScript is Script {
+    using stdJson for string;
+    using ScriptTools for string;
+
+    string constant NAME = "single-stusds-line-or-cap-wipe-deploy";
+    string config;
+
+    StUsdsSingleLineOrCapWipeFactory fab;
+
+    function run() external {
+        config = ScriptTools.loadConfig();
+
+        fab = StUsdsSingleLineOrCapWipeFactory(config.readAddress(".factory", "FOUNDRY_FACTORY"));
+
+        vm.startBroadcast();
+
+        ScriptTools.exportContract(NAME, "STUSDS_LINE", fab.deploy(Flow.LINE));
+        ScriptTools.exportContract(NAME, "STUSDS_BUY", fab.deploy(Flow.CAP));
+        ScriptTools.exportContract(NAME, "STUSDS_BOTH", fab.deploy(Flow.BOTH));
+    
+        vm.stopBroadcast();
+    }
+}
